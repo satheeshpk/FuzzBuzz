f9131b2acfc6941a1a57d2be0f43fedfedb4a40e
Satheesh Kumar <satheesh.kumar@swordtsl.com>
2017-02-15 22:59:18 +0000

    Added PHM.Maths.Words.Core Project and created IWordService Interface

---
+++ PHM.Maths.Words.Services.Core/IWordService.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IWordService.cs" company="PHM">
+//     Copyright (c) PHM. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PHM.Maths.Words.Services.Core
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Word Service Contract
+    /// </summary>
+    /// <typeparam name="T">type parameter</typeparam>
+    public interface IWordService<T>
+    {
+        /// <summary>
+        /// Gets the word.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>the word</returns>
+        Task<string> GetWord(T value);
+    }
+}
