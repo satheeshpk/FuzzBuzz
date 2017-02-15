7e27b36c3d61df7460255c438b5f38b54912c7ca
Satheesh Kumar <satheesh.kumar@swordtsl.com>
2017-02-15 23:59:51 +0000

    Created PHM.Math.Words.Services and PHM.Math.Words.Services.Test project. Also Added FuzzBuzzWordService (service) and FizzBuzzWordServiceTest (unit test for service).

---
+++ PHM.Math.Words.Services.Tests/FizzBuzzWordServiceTest.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="FizzBuzzWordServiceTest.cs" company="PHM">
+//     Copyright (c) PHM. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PHM.Math.Words.Services.Tests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Fizz buzz word service test
+    /// </summary>
+    [TestFixture]
+    public class FizzBuzzWordServiceTest
+    {
+    }
+}
---
+++ PHM.Math.Words.Services/FuzzBuzzWordService.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="FuzzBuzzWordService.cs" company="PHM">
+//     Copyright (c) PHM. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PHM.Math.Words.Services
+{
+    /// <summary>
+    /// Fuzz Buzz word service
+    /// </summary>
+    public class FuzzBuzzWordService
+    {
+    }
+}
