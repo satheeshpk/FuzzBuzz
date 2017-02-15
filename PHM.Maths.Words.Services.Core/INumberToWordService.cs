48ffc7a7f3e046e0f49ca7ae2ba758c58ea7cc9d
Satheesh Kumar <satheesh.kumar@swordtsl.com>
2017-02-15 23:02:29 +0000

    Added INumberToWordService Interface for abstracting number to word services

---
+++ PHM.Maths.Words.Services.Core/INumberToWordService.cs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="INumberToWordService.cs" company="PHM">
+//     Copyright (c) PHM. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PHM.Maths.Words.Services.Core
+{
+    /// <summary>
+    /// Number to word service contract
+    /// </summary>
+    /// <seealso cref="PHM.Maths.Words.Services.Core.IWordService{System.Int32}" />
+    public interface INumberToWordService : IWordService<int>
+    {
+    }
+}
