0f1307ee982603796b84c10950a5b5e8943e38b1
Satheesh Kumar <satheesh.kumar@swordtsl.com>
2017-02-15 22:39:26 +0000

    Project Created

--- PHM.Math.Words/PHM.Math.Words/App.cs
+++ PHM.Math.Words/PHM.Math.Words/App.cs
@@ -1,48 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
-
-namespace PHM.Math.Words
+﻿namespace PHM.Math.Words
 {
+    using Xamarin.Forms;
+
     public class App : Application
     {
         public App()
-        {
-            // The root page of your application
-            var content = new ContentPage
-            {
-                Title = "PHM.Math.Words",
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
-
-            MainPage = new NavigationPage(content);
-        }
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
+        {   
         }
     }
 }
