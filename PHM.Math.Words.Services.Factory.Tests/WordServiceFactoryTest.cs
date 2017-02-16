5549b99bdc652bc32bea4e1033bc83b3ac969d38
Satheesh Kumar <satheesh.kumar@swordtsl.com>
2017-02-16 00:34:55 +0000

    1. Implemented Word service factory. 2. Unit test of the Word service factory class.

---
+++ PHM.Math.Words.Services.Factory.Tests/WordServiceFactoryTest.cs
@@ -0,0 +1,51 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="WordServiceFactoryTest.cs" company="PHM">
+//     Copyright (c) PHM. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PHM.Math.Words.Services.Factory.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using Services;
+
+    /// <summary>
+    /// Word service factory test
+    /// </summary>
+    [TestFixture]
+    public class WordServiceFactoryTest
+    {
+        /// <summary>
+        /// Should the word service factory provide fizz buzz service when fizz buzz feed service is requested.
+        /// </summary>
+        [Test]
+        public void Should_WordServiceFactory_Provide_FizzBuzzService_When_FizzBuzz_ServiceType()
+        {
+            var numberWordService = new WordServiceFactory().GetNumberWordService("FizzBuzz");
+            Assert.IsInstanceOf(typeof(FizzBuzzWordService), numberWordService);
+        }
+
+        /// <summary>
+        /// Should the word service throw keynotfound exception when invalid service type is specified
+        /// </summary>
+        [Test]
+        public void Should_WordServiceFactory_Throw_KeyNotFoundException_When_Invalid_ServiceType()
+        {
+            var factory = new WordServiceFactory();
+            Assert.Throws<KeyNotFoundException>(() => factory.GetNumberWordService(""));
+            Assert.Throws<KeyNotFoundException>(() => factory.GetNumberWordService("TestFeed"));
+        }
+
+        /// <summary>
+        /// Should the word service throw keynotfound exception when service type is null
+        /// </summary>
+        [Test]
+        public void Should_WordServiceFactory_Throw_ArgumentNullException_When_Null_ServiceType()
+        {
+            var factory = new WordServiceFactory();
+            Assert.Throws<ArgumentNullException>(() => factory.GetNumberWordService(null));
+        }
+    }
+}
---
+++ PHM.Math.Words.Services.Factory/WordServiceFactory.cs
@@ -0,0 +1,47 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="WordServiceFactory.cs" company="PHM">
+//     Copyright (c) PHM. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PHM.Math.Words.Services.Factory
+{   
+    using System.Collections.Generic;
+    using Core;
+    using Services;
+
+    /// <summary>
+    /// Word Service Factory
+    /// </summary>
+    public class WordServiceFactory
+    {
+        /// <summary>
+        /// The number word services
+        /// </summary>
+        private Dictionary<string, INumberToWordService> numberWordServices = new Dictionary<string, INumberToWordService>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WordServiceFactory"/> class.
+        /// </summary>
+        public WordServiceFactory()
+        {
+            this.numberWordServices.Add("FizzBuzz", new FizzBuzzWordService());
+        }
+
+        /// <summary>
+        /// Gets the number word service.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <returns>the number service</returns>
+        /// <exception cref="System.Collections.Generic.KeyNotFoundException">No associated service type was found.</exception>
+        public INumberToWordService GetNumberWordService(string serviceType)
+        {
+            if (!this.numberWordServices.ContainsKey(serviceType))
+            {
+                throw new KeyNotFoundException("No associated service type was found.");
+            }
+
+            return this.numberWordServices[serviceType];
+        }
+    }
+}
